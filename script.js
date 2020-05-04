50598c30d34798e42b90d0a02ec6289b6aa2fe38
jack <43568b@gmail.com>
2020-05-04 11:38:08 -0400

    working etch a sketch

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ for (i = 0; i <= 16; i++) {
     for (e = 0; e <= 16; e++) {
         const square = document.createElement("div");
         square.classList.add("unit")
-        // square.textContent = (`${i},${e}`);
 
         column.appendChild(square);
     }
     container.appendChild(column);
 }
+
+const units = document.querySelectorAll('.unit');
+
+units.forEach((unit => {
+    unit.addEventListener('mouseover', (e) => {
+        unit.style.backgroundColor = "red";
+    })
+}))
+
+const reset = document.querySelector('.reset')
+
+reset.addEventListener('click', (e) => {
+    units.forEach(unit => {unit.style.backgroundColor = "pink"})
+});
